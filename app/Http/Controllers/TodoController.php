7e3aeec8a9bf46f217674904b4a035a8138cdea5
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-03-28 14:21:46 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todos = Todo::all();
+        //$todos = Todo::where('user_id', auth()->id())->get();
+        dd($todos);
         return view('todo.index');
     }
     public function create()
